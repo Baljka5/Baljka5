3983469408be11667677c60292bd04ea0ec34f8f
Baljinnyam Yerenjin <baljinnyam.ye@cumongol.mn>
2023-02-02 15:12:50 +0800

    Rect project

---
+++ src/components/ToolBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import css from "./module.style.css";
+const ToolBar = () => (
+  <header className={css.ToolBar}>
+    <div>...</div>
+    <div className={css.Content}>Logo</div>
+    <nav>Menu</nav>
+  </header>
+);
+
+export default ToolBar;
